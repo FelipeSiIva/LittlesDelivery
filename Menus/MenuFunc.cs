686008fa3c65df91942352b799be4573d05fca06
Gustavo Azevedo <105758163+GustavoAzvdo@users.noreply.github.com>
2022-09-01 09:57:26 -0300

    Menus Adm e Func

---
+++ Menus/MenuFunc.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LittlesSoft
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirFecharCaixaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 caixa = new Form3();
+            caixa.Show();
+            this.Close();
+        }
+    }
+}
